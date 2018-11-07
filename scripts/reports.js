8898b4b5c4ab6d713fad123a0d9d41f6e308cd51
bjvickers <bjvickers@protonmail.com>
2018-11-07 10:00:25 +0000

    build: added coverage reporting

--- scripts/reports.js
+++
@@ -1,9 +0,0 @@
-const execa = require('execa')
-
-const clean = async () => {
-  await execa("rm", ["-rf", "reports"])
-  await execa("mkdir", ["-vp", "reports/complexity"])
-  await execa("mkdir", ["-vp", "reports/reminders"])
-}
-
-clean()
